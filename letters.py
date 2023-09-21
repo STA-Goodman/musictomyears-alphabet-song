dddd0f5b7a0039ba7d9e869204624d01162e4a3b
CameronConrad <143106570+CameronConrad@users.noreply.github.com>
2023-09-21 08:37:39 -0500

    Added notes and letters

---
+++ letters.py
@@ -0,0 +1,87 @@
+import winsound
+import time
+def a():
+    winsound.Beep(261, 500)
+    
+def b():
+    winsound.Beep(261, 500)
+     
+def c():
+    winsound.Beep(392, 500)
+    
+def d():
+    winsound.Beep(392, 500)
+  
+
+def e():
+    winsound.Beep(440, 500)
+     
+def f():
+    winsound.Beep(440, 500)
+    
+
+def g():
+    winsound.Beep(392, 1000)
+    
+def h():
+    winsound.Beep(342, 500)
+    
+def i():
+    winsound.Beep(342, 500)
+   
+
+def j():
+    winsound.Beep(329, 500)
+    
+def k():
+    winsound.Beep(329, 500)
+ 
+
+def l():
+    winsound.Beep(293, 250)
+    
+def m():
+    winsound.Beep(293, 250)
+   
+def n():
+    winsound.Beep(293, 250)
+   
+def o():
+    winsound.Beep(293, 250)
+   
+def p():
+    winsound.Beep(261, 1000)
+    
+def q():
+    winsound.Beep(392, 500)
+   
+def r():
+    winsound.Beep(392, 500)
+    
+def s():
+    winsound.Beep(349, 1000)
+    
+def t():
+    winsound.Beep(329, 500)
+    
+def u():
+    winsound.Beep(329, 500)
+    
+def v():
+    winsound.Beep(293, 1000)
+   
+def w():
+    winsound.Beep(392, 250)
+    winsound.Beep(392, 250)
+    winsound.Beep(392, 500)
+    
+def x():
+    winsound.Beep(349, 1000)
+   
+def y():
+    winsound.Beep(329, 500)
+    winsound.Beep(329, 500)
+   
+def z():
+    winsound.Beep(293, 1000)
+    
---
+++ notes.py
@@ -0,0 +1,49 @@
+import winsound
+
+# Play the note c
+def c():
+    winsound.Beep(262, 500)
+
+# Play the note c#
+def c_sharp():
+    winsound.Beep(277, 500)
+
+# Play the note d
+def d():
+    winsound.Beep(294, 500)
+
+# Play the note d#
+def d_sharp():
+    winsound.Beep(311, 500)
+
+# Play the note e
+def e():
+    winsound.Beep(330, 500)
+
+# Play the note f
+def f():
+    winsound.Beep(349, 500)
+
+# Play the note f#
+def f_sharp():
+    winsound.Beep(370, 500)
+
+# Play the note g
+def g():
+    winsound.Beep(392, 500)
+
+# Play the note g#
+def g_sharp():
+    winsound.Beep(415, 500)
+
+# Play the note a
+def a():
+    winsound.Beep(440, 500)
+
+# Play the note a#
+def a_sharp():
+    winsound.Beep(466, 500)
+
+# Play the note b
+def b():
+    winsound.Beep(494, 500)
